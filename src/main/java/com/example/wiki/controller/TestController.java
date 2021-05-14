d66fa8e126231033bdb07542bac0dc0e51d34362
midasism <1719671503@qq.com>
2021-05-14 21:58:38 +0800

    开发 test/list接口，查询test表所有数据

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,14 +1,22 @@
 package com.example.wiki.controller;
 
+import com.example.wiki.entity.Test;
+import com.example.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class TestController {
     @Value("${test.hello}")
     private String hello;
+
+     @Resource
+     private TestService testService;
 
     @RequestMapping("/hello")
     public String hello() {
@@ -18,6 +26,11 @@ public class TestController {
     @PostMapping("/hello/post")
     public String helloPost(String name) {
         return "Post Test:" + name;
+    }
+
+    @RequestMapping("/test/list")
+    public List<Test> list() {
+        return testService.list();
     }
 }
 
---
+++ src/main/java/com/example/wiki/mapper/TestMapper.java
@@ -0,0 +1,9 @@
+package com.example.wiki.mapper;
+
+import com.example.wiki.entity.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    public List<Test> list();
+}
