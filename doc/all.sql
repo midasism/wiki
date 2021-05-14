008e440b9ff4a171b7e157ad1d60e92cd5ab72e0
midasism <1719671503@qq.com>
2021-05-15 00:31:34 +0800

    集成mybatis-generator,生成demo表代码

--- doc/all.sql
+++ doc/all.sql
@@ -9,3 +9,15 @@ create table `test`
 
 insert into `test` (`id`,`name`,`password`) values(1,'a','123');
 insert into `test` (`id`,`name`,`password`) values(2,'b','456');
+
+#demo
+drop table if exists `demo`;
+create table `demo`
+(
+    `id`   bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+)engine = innodb default charset = utf8mb4 comment = 'mybatis-generator test';
+
+insert into `demo` (`id`,`name`) values(1,'a');
+insert into `demo` (`id`,`name`) values(2,'b');
