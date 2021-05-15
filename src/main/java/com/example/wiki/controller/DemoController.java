c3140584035dae431d0e576e5d164a9fa9ade8cc
midasism <1719671503@qq.com>
2021-05-15 08:06:03 +0800

    增加demo表查询

---
+++ src/main/java/com/example/wiki/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.wiki.controller;
+
+import com.example.wiki.entity.Demo;
+import com.example.wiki.service.DemoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+     @Resource
+     private DemoService demoService;
+
+
+    @RequestMapping("/list")
+    public List<Demo> list() {
+        return demoService.list();
+    }
+}
+
