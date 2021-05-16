4b5661c4c94f1a64adb71b80fdaef8c8ad60543b
midasism <1719671503@qq.com>
2021-05-16 13:42:14 +0800

    增加电子书管理页面 新页面开发：页面、路由、菜单

---
+++ web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    电子书管理
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import axios from 'axios';
+
+export default defineComponent({
+  name: 'AdminEbook',
+  components: {},
+  //vue3 初始化调用 组合式API的入口 包含了vue2的生命周期函数
+  setup() {
+    console.log("setup");
+
+    return {}
+  }
+});
+</script>
+
+<!--只在当前组件生效-->
+<style scoped>
+
+</style>
