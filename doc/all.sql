fa8c3634e9be4ed27f3c29779d9245ebf1c6d22c
midasism <1719671503@qq.com>
2021-05-26 08:39:46 +0800

    设计用户表，生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -130,18 +130,18 @@ create table `doc`
 ) engine = innodb
   default charset = utf8mb4 comment = '文档';
 
-    insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
-    values (1, 1, 0, '文档1', 1, 0, 0);
-    insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
-    values (2, 1, 1, '文档1.1', 1, 0, 0);
-    insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
-    values (3, 1, 0, '文档2', 2, 0, 0);
-    insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
-    values (4, 1, 3, '文档2.1', 1, 0, 0);
-    insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
-    values (5, 1, 3, '文档2.2', 2, 0, 0);
-    insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
-    values (6, 1, 5, '文档2.2.1', 1, 0, 0);
+insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
+values (1, 1, 0, '文档1', 1, 0, 0);
+insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
+values (2, 1, 1, '文档1.1', 1, 0, 0);
+insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
+values (3, 1, 0, '文档2', 2, 0, 0);
+insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
+values (4, 1, 3, '文档2.1', 1, 0, 0);
+insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
+values (5, 1, 3, '文档2.2', 2, 0, 0);
+insert into `doc`(`id`, `ebook_id`, `parent`, `name`, `sort`, `view_count`, `vote_count`)
+values (6, 1, 5, '文档2.2.1', 1, 0, 0);
 
 
 # 文档内容 content
@@ -152,8 +152,22 @@ create table `doc`
 drop table if exists `content`;
 create table `content`
 (
-    `id`         bigint not null comment '文档id',
-    `content`   mediumtext not null comment '内容',
+    `id`      bigint     not null comment '文档id',
+    `content` mediumtext not null comment '内容',
     primary key (`id`)
 ) engine = innodb
   default charset = utf8mb4 comment = '文档内容';
+
+
+    # 用户表
+    drop table if exists `user`;
+    create table `user`
+    (
+        `id`         bigint      not null comment 'ID',
+        `login_name` varchar(50) not null comment '登录名',
+        `name`       varchar(50) comment '昵称',
+        `password`   char(32)    not null comment '密码',
+        primary key (`id`),
+        unique key `login_name_unique` (`login_name`)
+    ) engine = innodb
+      default charset = utf8mb4 comment = '用户';
