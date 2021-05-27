30ade9f466b3a63f9a3838ec77037ddcc9e50f64
midasism <1719671503@qq.com>
2021-05-27 13:54:27 +0800

    前端增加路由拦截，未登录时跳转到首页:防止通过url直接访问

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,56 +1,89 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import Home from '../views/home.vue'
 import Doc from '../views/doc.vue'
 import AdminUser from '../views/admin/admin-user.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
+import store from "@/store";
+import {Tool} from "@/util/tool";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
-  },
-  {
-    path: '/doc',
-    name: 'Doc',
-    component: Doc
-  },
-  {
-    path: '/admin/user',
-    name: 'AdminUser',
-    component: AdminUser
-  },
-  {
-    path: '/admin/ebook',
-    name: 'AdminEbook',
-    component: AdminEbook
-  },
-  {
-    path: '/admin/category',
-    name: 'AdminCategory',
-    component: AdminCategory
-  },
-  {
-    path: '/admin/doc',
-    name: 'AdminDoc',
-    component: AdminDoc
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+    },
+    {
+        path: '/doc',
+        name: 'Doc',
+        component: Doc
+    },
+    {
+        path: '/admin/user',
+        name: 'AdminUser',
+        component: AdminUser,
+        meta: {
+            loginRequire: true
+        }
+    },
+    {
+        path: '/admin/ebook',
+        name: 'AdminEbook',
+        component: AdminEbook,
+        meta: {
+            loginRequire: true
+        }
+    },
+    {
+        path: '/admin/category',
+        name: 'AdminCategory',
+        component: AdminCategory,
+        meta: {
+            loginRequire: true
+        }
+    },
+    {
+        path: '/admin/doc',
+        name: 'AdminDoc',
+        component: AdminDoc,
+        meta: {
+            loginRequire: true
+        }
+    },
 
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+//路由登录拦截
+router.beforeEach((to, from, next) => {
+    //对meta.LoginRequire做拦截判断 如果为true 代表需要登录才能访问
+    if (to.matched.some(function (item) {
+        console.log(item, "是否需要做登录校验", item.meta.loginRequire)
+        return item.meta.loginRequire
+    })) {
+        const loginUser = store.state.user
+        if(Tool.isEmpty(loginUser)){
+            console.log("用户未登录")
+            next("/")
+        }else{
+            next()
+        }
+    } else {
+        next()
+    }
 })
 
 export default router
