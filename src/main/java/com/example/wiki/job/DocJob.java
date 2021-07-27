7d69953b63bdf859fdce9e20641c20e0ba86afdd
midasism <1719671503@qq.com>
2021-07-27 21:58:26 +0800

    增加日志流水号，方便查看调试

--- src/main/java/com/example/wiki/job/DocJob.java
+++ src/main/java/com/example/wiki/job/DocJob.java
@@ -1,8 +1,10 @@
 package com.example.wiki.job;
 
 import com.example.wiki.service.DocService;
+import com.example.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +18,15 @@ public class DocJob {
     @Resource
     DocService docService;
 
+    @Resource
+    SnowFlake snowFlake;
+
     /**
      * 每30秒更新电子书信息
      */
     @Scheduled(cron = "5/5 * * * * ?")
     public void cron()  {
+        MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
         LOG.info("开始更新电子书文档信息，执行sql");
         long start = System.currentTimeMillis();
         docService.updateEbookInfo();
